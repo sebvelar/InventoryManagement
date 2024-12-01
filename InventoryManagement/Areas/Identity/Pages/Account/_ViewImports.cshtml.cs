29cd0ed8e51aadd03612049035ca95e9f77a525e
ANCODE2000 <angieduranmartha@gmail.com>
2024-11-30 22:51:38 -0600

    Se agrega archivos faktantes de login y registro.

---
+++ InventoryManagement/Areas/Identity/Pages/Account/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryManagement.Areas.Identity.Pages.Account
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
